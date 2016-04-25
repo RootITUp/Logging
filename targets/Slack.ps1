f4b16a7465dab2ebe1eedb7f3eafaf9d52a04767
Ryan McVicar <mcvic1rj@cmich.edu>
2016-04-25 11:19:44 -0400

    Added a feature to log to slack

---
+++ targets/Slack.ps1
@@ -0,0 +1,43 @@
+@{
+    Name = 'Slack'
+    Logger = {
+        param(
+            $Log, 
+            $Format, 
+            $Configuration
+        )
+        
+        $Index = Replace-Tokens -String $Configuration.Index -Source $Log
+        $Uri = '{0}' -f  $Configuration.ServerURI
+        $text=@{text=$($log.msg.title + $log.msg.body)}  
+        if ($log.msg.body)
+        {
+            $text=@{text=$($log.msg.title +": " +$log.msg.body)}
+        }
+        else 
+        {
+            $text=@{text=$log.msg.title}        
+        }
+        if ($Configuration.BotName)
+        {
+          $text.Add("username",$Configuration.BotName)    
+        }    
+        if ($Configuration.Channel)
+        {
+            $text.Add("channel",$Configuration.Channel)
+        }
+        if ($log.levelno -ge 30 -and $log.levelno -lt 40)
+        {
+            $text.Add("icon_emoji",":warning:")
+        }
+        elseif ($log.levelno -ge 40)
+        {
+            $text.Add("icon_emoji",":fire:")
+        }
+        else 
+        {
+            $text.Add("icon_emoji",":exclamation:")
+        }
+        Invoke-RestMethod -Method Post -Uri $Uri -Body ($text|ConvertTo-Json)
+    }
+}
