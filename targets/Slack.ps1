d9966718e8ab033cd3408594914fce7a03bc0dd1
Massimo Bonvicini <massimo.bonvicini@gmail.com>
2016-04-26 15:57:56 +0200

    code cleanup and configuration section added

--- targets/Slack.ps1
+++ targets/Slack.ps1
@@ -1,37 +1,36 @@
 @{
     Name = 'Slack'
+    Configuration = @{
+        ServerUri = @{Required = $true; Type = [string]}
+        BotName = @{Required = $false; Type = [string]}
+        Channel = @{Required = $false; Type = [string]}
+        Level = @{Required = $false; Type = [string]}
+    }
     Logger = {
         param(
             $Log, 
             $Format, 
             $Configuration
         )
+                
+        $Text = @{
+            Text = '{0}' -f $Log.msg.Title
+        }
         
-        $Index = Replace-Tokens -String $Configuration.Index -Source $Log
-        $Uri = '{0}' -f  $Configuration.ServerURI
-        $text = @{text = $($log.msg.title + $log.msg.body)}  
-        if ($log.msg.body) {
-            $text = @{text = $($log.msg.title +": " +$log.msg.body)}
+        if ($Log.msg.Body.Count -ne 0) { $Text.Text += ': {0}' -f $Log.msg.Body | ConvertTo-Json}
+        
+        if ($Configuration.BotName) { $Text['username'] = $Configuration.BotName }
+        
+        if ($Configuration.Channel) { $Text['channel'] = $Configuration.Channel }
+        
+        if ($Log.levelno -ge 30 -and $Log.levelno -lt 40) {
+            $Text['icon_emoji'] = ':warning:'
+        } elseif ($Log.levelno -ge 40) {
+            $Text['icon_emoji'] = ':fire:'
         } else {
-            $text=@{text = $log.msg.title}        
+            $Text['icon_emoji'] = ':exclamation:'
         }
-        
-        if ($Configuration.BotName) {
-          $text.Add("username", $Configuration.BotName)    
-        }
-        
-        if ($Configuration.Channel) {
-            $text.Add("channel", $Configuration.Channel)
-        }
-        
-        if ($log.levelno -ge 30 -and $log.levelno -lt 40) {
-            $text.Add("icon_emoji",":warning:")
-        } elseif ($log.levelno -ge 40) {
-            $text.Add("icon_emoji",":fire:")
-        } else {
-            $text.Add("icon_emoji",":exclamation:")
-        }
-        
-        Invoke-RestMethod -Method Post -Uri $Uri -Body ($text|ConvertTo-Json)|out-null
+                 
+        Invoke-RestMethod -Method Post -Uri $Configuration.ServerUri -Body ($Text | ConvertTo-Json) | Out-Null
     }
 }
