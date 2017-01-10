ab11ab2a5eebeacd3da1b4319af53dc71d03e948
Massimo Bonvicini <massimo.bonvicini@gmail.com>
2017-01-10 16:18:34 +0100

    Fixed publishing only on master branch

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ param (
 )
 
 if ($env:APPVEYOR) {
-    if ($env:APPVEYOR_REPO_TAG) {
+    if ($env:APPVEYOR_REPO_TAG -and $env:APPVEYOR_REPO_BRANCH -eq 'master') {
         Invoke-psake .\build.psake.ps1 -taskList Release -nologo
     } else {
         Invoke-psake .\build.psake.ps1 -nologo
