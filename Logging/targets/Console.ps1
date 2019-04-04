d96a5a91bd16ef2677fa48d5ab2e0d29711a1802
Massimo Bonvicini <massimo.bonvicini@unipolsai.it>
2019-04-04 16:49:07 +0200

    Added default config + minor rework

--- Logging/targets/Console.ps1
+++ Logging/targets/Console.ps1
@@ -2,10 +2,32 @@
     Name = 'Console'
     Description = 'Writes messages to console with different colors.'
     Configuration = @{
-        Level        = @{Required = $false; Type = [string]}
-        Format       = @{Required = $false; Type = [string]}
-        ColorMapping = @{Required = $false; Type = [hashtable]}
+        Level        = @{Required = $false; Type = [string];    Default = Get-LoggingDefaultLevel}
+        Format       = @{Required = $false; Type = [string];    Default = Get-LoggingDefaultFormat}
+        ColorMapping = @{Required = $false; Type = [hashtable]; Default = @{
+                                                                    'DEBUG'   = 'Blue'
+                                                                    'INFO'    = 'Green'
+                                                                    'WARNING' = 'Yellow'
+                                                                    'ERROR'   = 'Red'
+                                                                }
+        }
     }
+
+    Init = {
+        param(
+            [hashtable] $Configuration
+        )
+
+        foreach ($Level in $Configuration.ColorMapping.Keys) {
+            $Color = $Configuration.ColorMapping[$Level]
+
+            if ($Color -notin ([System.Enum]::GetNames([System.ConsoleColor]))) {
+                $ParentHost.UI.WriteErrorLine("ERROR: Cannot use custom color '$Color': not a valid [System.ConsoleColor] value")
+                continue
+            }
+        }
+    }
+
     Logger = {
         param(
             $Log,
@@ -13,28 +35,7 @@
             $Configuration
         )
 
-        $ColorMapping = @{
-            'DEBUG'   = 'Blue'
-            'INFO'    = 'Green'
-            'WARNING' = 'Yellow'
-            'ERROR'   = 'Red'
-        }
-
-        if ($Configuration.ColorMapping) {
-            foreach ($Level in $Configuration.ColorMapping.Keys) {
-                $Color = $Configuration.ColorMapping[$Level]
-
-                if ($Color -notin ([System.Enum]::GetNames([System.ConsoleColor]))) {
-                    $ParentHost.UI.WriteErrorLine("ERROR: Cannot use custom color '$Color': not a valid [System.ConsoleColor] value")
-                    continue
-                }
-
-                $ColorMapping[$Level] = $Configuration.ColorMapping[$Level]
-            }
-        }
-
         $mtx = New-Object System.Threading.Mutex($false, 'ConsoleMtx')
-        $mtx.WaitOne()
 
         $Text = Replace-Token -String $Format -Source $Log
 
@@ -42,12 +43,10 @@
             $Text += "`n" + $Log.ExecInfo.InvocationInfo.PositionMessage
         }
 
-        $OldColor = $ParentHost.UI.RawUI.ForegroundColor
-        $ParentHost.UI.RawUI.ForegroundColor = $ColorMapping[$Log.Level]
-        $ParentHost.UI.WriteLine($Text)
-        $ParentHost.UI.RawUI.ForegroundColor = $OldColor
-
+        $mtx.WaitOne()
+        $ParentHost.UI.WriteLine($Configuration.ColorMapping[$Log.Level], $ParentHost.UI.RawUI.BackgroundColor, $Text)
         [void] $mtx.ReleaseMutex()
+
         $mtx.Dispose()
     }
 }
