c66cc154673bb55925eec5d670e9bf4647da4c02
Massimo Bonvicini <massimo.bonvicini@unipolsai.it>
2019-04-08 09:32:44 +0200

    code format updates

--- Logging/private/Set-LoggingVariables.ps1
+++ Logging/private/Set-LoggingVariables.ps1
@@ -1,46 +1,45 @@
-function Set-LoggingVariables{
-    param()
+function Set-LoggingVariables {
 
     #Already setup
-    if($Logging -and $LogTargets -and $LevelNames){
+    if ($Logging -and $LogTargets -and $LevelNames) {
         return
     }
 
     Write-Verbose -Message ("{0} :: Initial variable setup.")
-    
-    $Script:NOTSET     = 0
-    $Script:DEBUG      = 10
-    $Script:INFO       = 20
-    $Script:WARNING    = 30
-    $Script:ERROR_     = 40
+
+    $Script:NOTSET = 0
+    $Script:DEBUG = 10
+    $Script:INFO = 20
+    $Script:WARNING = 30
+    $Script:ERROR_ = 40
 
     $initialLevels = [hashtable]::Synchronized(@{
-        $NOTSET = 'NOTSET'
-        $ERROR_ = 'ERROR'
-        $WARNING = 'WARNING'
-        $INFO = 'INFO'
-        $DEBUG = 'DEBUG'
-        'NOTSET' = $NOTSET
-        'ERROR' = $ERROR_
+        $NOTSET   = 'NOTSET'
+        $ERROR_   = 'ERROR'
+        $WARNING  = 'WARNING'
+        $INFO     = 'INFO'
+        $DEBUG    = 'DEBUG'
+        'NOTSET'  = $NOTSET
+        'ERROR'   = $ERROR_
         'WARNING' = $WARNING
-        'INFO' = $INFO
-        'DEBUG' = $DEBUG
+        'INFO'    = $INFO
+        'DEBUG'   = $DEBUG
     })
 
-    New-Variable -Name LevelNames   -Scope Script -Value $initialLevels -Option Constant
-    New-Variable -Name Logging      -Scope Script -Value ([hashtable]::Synchronized(@{})) -Option Constant
-    New-Variable -Name LogTargets   -Scope Script -Value ([hashtable]::Synchronized(@{})) -Option Constant
-    New-Variable -Name ScriptRoot   -Scope Script -Value ([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Module.Path)) -Option Constant
-    New-Variable -Name Defaults -Scope Script -Option Constant -Value @{
+    New-Variable -Name LevelNames   -Scope Script -Option Constant -Value $initialLevels
+    New-Variable -Name Logging      -Scope Script -Option Constant -Value ([hashtable]::Synchronized(@{}))
+    New-Variable -Name LogTargets   -Scope Script -Option Constant -Value ([hashtable]::Synchronized(@{}))
+    New-Variable -Name ScriptRoot   -Scope Script -Option Constant -Value ([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Module.Path))
+    New-Variable -Name Defaults     -Scope Script -Option Constant -Value @{
         Level       = $Script:NOTSET
         Format      = '[%{timestamp:+%Y-%m-%d %T%Z}] [%{level:-7}] %{message}'
         Timestamp   = '%Y-%m-%dT%T%Z'
         CallerScope = 1
     }
-    
-    $Script:Logging.Level      = $Defaults.Level
-    $Script:Logging.Format     = $Defaults.Format
+
+    $Script:Logging.Level = $Defaults.Level
+    $Script:Logging.Format = $Defaults.Format
     $Script:Logging.CallerScope = $Defaults.CallerScope
-    $Script:Logging.Targets    = [hashtable]::Synchronized(@{})
+    $Script:Logging.Targets = [hashtable]::Synchronized(@{})
     $Script:Logging.CustomTargets = [String]::Empty
 }
