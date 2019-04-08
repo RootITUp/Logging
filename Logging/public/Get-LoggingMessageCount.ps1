eaa6ff171bb6dc77ef4774eafa4f97db8229e528
Massimo Bonvicini <massimo.bonvicini@unipolsai.it>
2019-04-08 10:31:01 +0200

    added doc

--- Logging/public/Get-LoggingMessageCount.ps1
+++ Logging/public/Get-LoggingMessageCount.ps1
@@ -1,24 +1,33 @@
 <#
-.SYNOPSIS
-Returns the currently processed log message count
+    .SYNOPSIS
+        Returns the currently processed log message count
 
-.DESCRIPTION
-When a message is processed by any log target it will be added
-to the count of logged messages.
-Get-LoggingMessageCount retrieves the sum of those messages.
+    .DESCRIPTION
+        When a message is processed by any log target it will be added
+        to the count of logged messages.
+        Get-LoggingMessageCount retrieves the sum of those messages.
 
-.EXAMPLE
-Set-LoggingDefaultLevel -Level ERROR
-Add-LoggingTarget -Name Console
-write-Log -Message "Test1"
-Write-Log -Message "Test2" -Level ERROR
+    .EXAMPLE
+        Set-LoggingDefaultLevel -Level ERROR
+        Add-LoggingTarget -Name Console
+        write-Log -Message "Test1"
+        Write-Log -Message "Test2" -Level ERROR
 
-Get-LoggingMessageCount
-=> 1
+        Get-LoggingMessageCount
+        => 1
+    .LINK
+        https://logging.readthedocs.io/en/latest/functions/Get-LoggingMessageCount.md
+
+    .LINK
+        https://logging.readthedocs.io/en/latest/functions/Write-Log.md
+
+    .LINK
+        https://github.com/EsOsO/Logging/blob/master/Logging/public/Get-LoggingMessageCount.ps1
+
 #>
 
 function Get-LoggingMessageCount {
-    [CmdletBinding()]
+    [CmdletBinding(HelpUri = 'https://logging.readthedocs.io/en/latest/functions/Get-LoggingMessageCount.md')]
     param()
 
     if (!(Get-Variable -Name "LoggingMessagerCount" -Scope Script -ErrorAction Ignore)) {
