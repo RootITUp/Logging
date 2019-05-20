d2cd31e14e76c49dc7d7134b2fba5003ee648f16
EsOsO <massimo.bonvicini@gmail.com>
2019-05-20 10:00:15 +0200

    sticking to psake 4.7.4

--- build.ps1
+++ build.ps1
@@ -5,9 +5,12 @@ param (
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, BuildHelpers, platyPS, PSScriptAnalyzer -Force
+Install-Module -Name Psake -RequiredVersion '4.7.4'
+Install-Module PSDeploy, BuildHelpers, platyPS, PSScriptAnalyzer -Force
 Install-Module Pester -Force -SkipPublisherCheck
-Import-Module Psake, BuildHelpers, platyPS, PSScriptAnalyzer
+
+Import-Module Psake -RequiredVersion '4.7.4'
+Import-Module BuildHelpers, platyPS, PSScriptAnalyzer
 
 Set-BuildEnvironment
 Get-Module
