27065ec6323eb284b11bf02a5cc447dda93408aa
Florian Huber <Florian.Huber@frauscher.com>
2020-03-11 20:56:02 +0100

    Implemented and Tested Webex Teams Target

--- Logging/targets/WebexTeams.ps1
+++ Logging/targets/WebexTeams.ps1
@@ -1,4 +1,4 @@
-@{
+﻿@{
     Name = 'WebexTeams'
     Configuration = @{
         BotToken = @{Required = $true; Type = [string]; Default = $null }
@@ -18,19 +18,18 @@
             [hashtable] $Log,
             [hashtable] $Configuration
         )
-        
+        # Build the Message body
         $body = @{
-          roomId=$Configuration.RoomID
-          text=$Configuration.Icons[$Log.LevelNo]+" "+Replace-Token -String $Configuration.Format -Source $Log
+          roomId=$Configuration.RoomId
+          text=$Configuration.Icons[$Log.Level]+" "+$(Replace-Token -String $Configuration.Format -Source $Log)
         }
 
+        # Convert to JSON
         $json=$body | ConvertTo-Json
-        
+        # Send Message to Cisco Webex API - UTF8 Handling for Emojiis
         Invoke-RestMethod -Method Post `
-          -Headers @{"Authorization"="Bearer $Configuration.BotToken"} `
+          -Headers @{"Authorization"="Bearer $($Configuration.BotToken)"} `
           -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($json)) `
           -Uri "https://api.ciscospark.com/v1/messages"
-
-        
     }
 }
