399a8db611cfe7357ce7af50d3a368ee116711ab
Florian Huber <florian.huber@itshorty.at>
2020-03-19 10:31:53 +0100

    Cisco Webex Teams Target (#80)
    * Created Webex Teams Target
    
    Based on Slack.ps1
    
    * Update WebexTeams.ps1
    
    * Implemented and Tested Webex Teams Target

---
+++ Tests/WebexTeams.Tests.ps1
@@ -0,0 +1,41 @@
+﻿Remove-Module Logging -Force -ErrorAction SilentlyContinue
+
+$ModuleManifestPath = '{0}\..\Logging\Logging.psd1' -f $PSScriptRoot
+Import-Module $ModuleManifestPath -Force
+
+$TargetFile = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.ps1'
+$TargetImplementationPath = '{0}\..\Logging\targets\{1}' -f $PSScriptRoot, $TargetFile
+
+Describe -Tags Targets, TargetWebexTeams 'WebexTeams target' {
+    It 'should be available in the module' {
+        $Targets = Get-LoggingAvailableTarget
+        $Targets.WebexTeams | Should Not BeNullOrEmpty
+    }
+
+    It 'should have two required parameters' {
+        $Targets = Get-LoggingAvailableTarget
+        $Targets.WebexTeams.ParamsRequired | Should Be @('BotToken', 'RoomID')
+    }
+
+    It 'should call Invoke-RestMethod' {
+        Mock Invoke-RestMethod -Verifiable
+
+        $Module = . $TargetImplementationPath
+
+        $Message = [hashtable] @{
+            level   = 'ERROR'
+            levelno = 40
+            message = 'Hello, WebexTeams!'
+        }
+
+        $Configuration = @{
+            BotToken = 'SOMEINVALIDTOKEN'
+            RoomID = 'SOMEINVALIDROOMID'
+            Icons = $Module.Configuration.Icons.Default
+        }
+
+        & $Module.Logger $Message $Configuration
+
+        Assert-MockCalled -CommandName 'Invoke-RestMethod' -Times 1 -Exactly
+    }
+}
