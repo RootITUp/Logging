b8298a6e64cecaa683e25ba0b13dd602fa137644
EsOsO <massimo.bonvicini@gmail.com>
2020-04-23 14:25:31 +0200

    force module upgrade

--- build.ps1
+++ build.ps1
@@ -6,8 +6,8 @@ param (
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Install-Module -Name BuildHelpers -RequiredVersion '2.0.11' -Scope CurrentUser
-Install-Module -Name Pester -SkipPublisherCheck -Scope CurrentUser
-Install-Module Psake, PSDeploy, platyPS, PSScriptAnalyzer -Scope CurrentUser
+Install-Module -Name Pester -SkipPublisherCheck -Scope CurrentUser -Force -RequiredVersion '4.10.1'
+Install-Module Psake, PSDeploy, platyPS, PSScriptAnalyzer -Scope CurrentUser -Force
 
 Import-Module Psake, BuildHelpers, platyPS, PSScriptAnalyzer
 
