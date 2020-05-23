19390889b26af9530f6ea6d29f7c7a6967df93b0
Tadas <thetadas+github@gmail.com>
2020-05-23 20:58:39 +0100

    Attempt to fix the build

--- Logging/private/Stop-LoggingManager.ps1
+++ Logging/private/Stop-LoggingManager.ps1
@@ -8,7 +8,10 @@ function Stop-LoggingManager {
     [void] $Script:LoggingRunspace.Powershell.Dispose()
 
     $ExecutionContext.SessionState.Module.OnRemove = $null
-    Get-EventSubscriber | Where-Object { $_.Action.Id -eq $Script:LoggingRunspace.EngineEventJob.Id } | Unregister-Event
+    Get-EventSubscriber | ForEach-Object {
+        Write-Host "Current Action.Id $($_.Action.Id); Looking for id: $($Script:LoggingRunspace.EngineEventJob.Id)"
+        $_
+    } | Where-Object { $_.Action.Id -eq $Script:LoggingRunspace.EngineEventJob.Id } | Unregister-Event
 
     Remove-Variable -Scope Script -Force -Name LoggingEventQueue
     Remove-Variable -Scope Script -Force -Name LoggingRunspace
