4a60a4b29e9a11bdccb3e6724db2f13df43ce6a8
Tadas <thetadas+github@gmail.com>
2020-06-02 23:47:31 +0100

    Remove debug output

--- Logging/private/Stop-LoggingManager.ps1
+++ Logging/private/Stop-LoggingManager.ps1
@@ -8,10 +8,7 @@ function Stop-LoggingManager {
     [void] $Script:LoggingRunspace.Powershell.Dispose()
 
     $ExecutionContext.SessionState.Module.OnRemove = $null
-    Get-EventSubscriber | ForEach-Object {
-        Write-Host "Current Action.Id $($_.Action.Id); Looking for id: $($Script:LoggingRunspace.EngineEventJob.Id)"
-        $_
-    } | Where-Object { $_.Action.Id -eq $Script:LoggingRunspace.EngineEventJob.Id } | Unregister-Event
+    Get-EventSubscriber | Where-Object { $_.Action.Id -eq $Script:LoggingRunspace.EngineEventJob.Id } | Unregister-Event
 
     Remove-Variable -Scope Script -Force -Name LoggingEventQueue
     Remove-Variable -Scope Script -Force -Name LoggingRunspace
