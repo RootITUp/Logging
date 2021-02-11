68a2e8eed5f2b36b7274ccbb8d84da9f19678599
EsOsO <massimo.bonvicini@gmail.com>
2021-02-11 14:55:40 +0100

    working toward structured logging #98

--- Logging/public/Write-Log.ps1
+++ Logging/public/Write-Log.ps1
@@ -66,12 +66,6 @@ Function Write-Log {
     }
 
     End {
-        [string] $messageText = $Message
-
-        if ($PSBoundParameters.ContainsKey('Arguments')) {
-            $messageText = $messageText -f $Arguments
-        }
-
         $levelNumber = Get-LevelNumber -Level $PSBoundParameters.Level
         $invocationInfo = (Get-PSCallStack)[$Script:Logging.CallerScope]
 
@@ -90,10 +84,16 @@ Function Write-Log {
             pathname     = $invocationInfo.ScriptName
             filename     = $fileName
             caller       = $invocationInfo.Command
-            message      = $messageText
+            message      = [string] $Message
+            rawmessage   = [string] $Message
             body         = $Body
             execinfo     = $ExceptionInfo
             pid          = $PID
+        }
+
+        if ($PSBoundParameters.ContainsKey('Arguments')) {
+            $logMessage["message"] = [string] $Message -f $Arguments
+            $logMessage["args"] = $Arguments
         }
 
         #This variable is initiated via Start-LoggingManager
